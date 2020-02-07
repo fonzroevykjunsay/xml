fd4d58639b102736f644ee61ccec3a27ee9543b8
fonzroevykjunsay <60122963+fonzroevykjunsay@users.noreply.github.com>
2020-02-07 21:47:29 +0800

    Create rss.php

---
+++ rss.php
@@ -0,0 +1,34 @@
+<?php 
+    $conn = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234") or die (mysqli_error($conn));
+    $db = mysqli_select_db($conn, "db_1820697");
+
+    if(mysqli_connect_errno($conn)){
+        echo "Database connection failed!: ". mysqli_connect_errno();
+    }
+    $sql = "SELECT * FROM tbl_bag";
+    $q = mysqli_query($conn, $sql);
+
+    header("Content-type: text/xml");
+
+    echo "<?xml version='1.0' encoding='UTF-8'?>
+        <rss version='2.0'><channel>";
+    
+    while($r = mysqli_fetch_array($q)){
+
+        $brand = $r['brand'];
+        $description = $r['description'];
+        $capacity = $r['capacity'];
+        $color = $r['color'];
+
+        echo "<bag>
+        <brand>$brand</brand>
+        <cost>$cost</cost>
+        <year>$year</year>
+        <color>$color</color>
+        </bag>";
+    }
+    echo "</channel></rss>";
+?>
+
+
+
